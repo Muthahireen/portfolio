e7041bc3068fa29d9b731fc4b2df1ee9e97858f0
Muthahireen <116145859+Muthahireen@users.noreply.github.com>
2023-11-27 19:38:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var Typed=new Typed(".text", {
+    strings: [  "Student" ,"Python code enthusiast" ,"web developer"],
+    TypeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
